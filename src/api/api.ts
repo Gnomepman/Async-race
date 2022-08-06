b790b1d88635a99e85f8ca133f9b7dd111cd41e9
Danya <danilomaster2003@ukr.net>
2022-08-06 23:31:30 +0300

    fix: fix api method

--- src/api/api.ts
+++ src/api/api.ts
@@ -35,12 +35,12 @@ export const updateCar = async (id: number, body: carBody) => (await fetch(`${ga
     }
 })).json();
 
-export const startEngine = async (id: number) => (await fetch(`${engine}?id=${id}&status=started`)).json();
+export const startEngine = async (id: number) => (await fetch(`${engine}?id=${id}&status=started`, {method: 'PATCH'})).json();
 
-export const stopEngine = async (id: number) => (await fetch(`${engine}?id=${id}&status=stopped`)).json();
+export const stopEngine = async (id: number) => (await fetch(`${engine}?id=${id}&status=stopped`, {method: 'PATCH'})).json();
 
 export const drive = async (id: number) => {
-    const res = await fetch(`${engine}?id=${id}&status=drive`).catch();
+    const res = await fetch(`${engine}?id=${id}&status=drive`, {method: 'PATCH'}).catch();
     return res.status !== 200 ? {success: false} : {...(await res.json())};
 }
 
