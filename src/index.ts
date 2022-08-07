dcf331206e110918eadd0e57a70c0f9eb95ca07d
Danya <danilomaster2003@ukr.net>
2022-08-07 12:59:25 +0300

    feat: generate html via js

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import './ui/initial_ui'
 import './style.css'
 import './ui/ui'
 import './events/events'
---
+++ src/ui/initial_ui.ts
@@ -0,0 +1,36 @@
+document.getElementById("body")!.innerHTML = `
+<div class="container">
+        <div class="navigation">
+            <input type="radio" name="select" value="garage" id="garage" checked>
+            <label for="garage">Garage</label>
+            <input type="radio" name="select" value="winners" id="winners">
+            <label for="winners">Winners</label>
+        </div>
+        <div class="garage-container">
+            <div class="customization">
+                <input type="text" name="" id="car-create-name">
+                <input type="color" name="" id="car-create-color" value="#4ea1c4">
+                <button class="button" id="create">Create</button>
+                <input type="text" name="" id="car-edit-name">
+                <input type="color" name="" id="car-edit-color" value="#ff0000">
+                <button class="button" id="update">Update</button>
+            </div>
+            <div class="controls">
+                <button id="race">Race</button>
+                <button id="reset">Reset</button>
+                <button id="generate">Generate cars</button>
+            </div>
+            <div class="garage">
+                <h2>Garage (<span id="garage-counter">0</span>)</h2>
+                <h3>Page #<span id="page-counter">1</span></h3>
+                <div id="garage-wrapper">
+                    <!-- cars will be here -->
+                </div>
+            </div>
+            <div class="controls">
+                <button id="prev_page">Previous</button>
+                <button id="next_page">Next</button>
+            </div>
+        </div>
+    </div>
+`
